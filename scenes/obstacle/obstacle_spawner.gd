f6f90e5359bb19cdbaad10d7c889513dec47b7ed
Jujube2005 <chotika.jakchai@gmail.com>
2026-06-25 21:51:52 +0700

    Restore obstacle_zone1 for Scene 1 (0-500m)

--- scenes/obstacle/obstacle_spawner.gd
+++ scenes/obstacle/obstacle_spawner.gd
@@ -33,15 +33,17 @@ func _get_zone_scene() -> PackedScene:
 		return obstacle_zone4
 	elif distance >= 1000.0:
 		return obstacle_zone3
-	else:
+	elif distance >= 500.0:
 		return obstacle_zone2
+	else:
+		return obstacle_zone1
 
 func _init_pool():
 	for i in range(pool_size):
-		var obs = obstacle_zone2.instantiate()
+		var obs = obstacle_zone1.instantiate()
 		obs.set_script(load("res://scenes/obstacle/obstacle.gd"))
 		obs.add_to_group("obstacle")
-		obs.set_meta("zone_scene", obstacle_zone2.resource_path)
+		obs.set_meta("zone_scene", obstacle_zone1.resource_path)
 		get_parent().add_child.call_deferred(obs)
 		obstacle_pool.append(obs)
 
