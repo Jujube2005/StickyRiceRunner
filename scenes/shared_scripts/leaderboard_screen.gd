acf7740e8e80460cb84741a4b231c137ba33d9a3
Jujube2005 <chotika.jakchai@gmail.com>
2026-06-26 18:37:20 +0700

    Split Leaderboard UI into distinct RaceUI and EndlessUI blocks, fix background texture

--- scenes/shared_scripts/leaderboard_screen.gd
+++ scenes/shared_scripts/leaderboard_screen.gd
@@ -1,19 +1,20 @@
 extends Control
 
-@onready var header_label: Label = %HeaderLabel
-
-@onready var winner_container: TextureRect = %WinnerContainer
-@onready var winner_tag: TextureRect = %WinnerTag
-@onready var avatar: TextureRect = %Avatar
-
-@onready var race_score_panel: TextureRect = %RaceScorePanel
+@onready var race_ui: VBoxContainer = %RaceUI
+@onready var race_header_label: Label = %RaceHeaderLabel
+@onready var race_winner_container: TextureRect = %RaceWinnerContainer
+@onready var race_winner_tag: TextureRect = %RaceWinnerTag
+@onready var race_avatar: TextureRect = %RaceAvatar
 @onready var val_p1_wins: Label = %ValP1Wins
 @onready var val_p2_wins: Label = %ValP2Wins
 
-@onready var endless_vbox: VBoxContainer = %EndlessVBox
+@onready var endless_ui: VBoxContainer = %EndlessUI
+@onready var endless_header_label: Label = %EndlessHeaderLabel
+@onready var endless_winner_container: TextureRect = %EndlessWinnerContainer
+@onready var endless_winner_tag: TextureRect = %EndlessWinnerTag
+@onready var endless_avatar: TextureRect = %EndlessAvatar
 @onready var val_dist_best: Label = %ValDistBest
 @onready var val_run: Label = %ValRun
-@onready var val_kratip: Label = %ValKratip
 
 @onready var btn_restart: TextureButton = %BtnRestart
 @onready var btn_menu: TextureButton = %BtnMenu
@@ -24,7 +25,6 @@ var tex_p1_avatar = preload("res://assets/textures/UI/Buttons/icon_player1Win.pn
 var tex_p2_avatar = preload("res://assets/textures/UI/Buttons/icon_player2Win.png")
 
 var default_scale: Vector2 = Vector2.ONE
-
 const SAVE_PATH = "user://leaderboard_data.json"
 
 static func load_data() -> Dictionary:
@@ -35,14 +35,7 @@ static func load_data() -> Dictionary:
 			var res = JSON.parse_string(text)
 			if res is Dictionary:
 				return res
-	return {
-		"best_distance": 0,
-		"best_time": 999999,
-		"most_silk": 0,
-		"p1_wins": 0,
-		"p2_wins": 0,
-		"top_3_times": []
-	}
+	return { "best_distance": 0, "best_time": 999999, "most_silk": 0, "p1_wins": 0, "p2_wins": 0, "top_3_times": [] }
 
 static func save_data(data: Dictionary):
 	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
@@ -51,10 +44,8 @@ static func save_data(data: Dictionary):
 func _ready():
 	hide()
 	default_scale = scale
-	
 	if btn_restart: btn_restart.pressed.connect(on_restart_pressed)
 	if btn_menu: btn_menu.pressed.connect(on_menu_pressed)
-	
 	if btn_restart: _setup_button_hover(btn_restart)
 	if btn_menu: _setup_button_hover(btn_menu)
 
@@ -72,55 +63,55 @@ func _setup_button_hover(btn: TextureButton):
 
 func show_result(winner_name: String, _p1_score: int, _p2_score: int, _p1_distance: int, _p2_distance: int):
 	var data = load_data()
-	
-	# Determine mode
 	var is_endless = GameConfig.race_mode == "endless"
 	
 	if is_endless:
-		if header_label: header_label.text = "Endless Result"
-		if winner_container: winner_container.hide()
-		if race_score_panel: race_score_panel.hide()
-		if endless_vbox: endless_vbox.show()
+		if race_ui: race_ui.hide()
+		if endless_ui: endless_ui.show()
 		
-		# Retrieve kratips from player if possible
-		var kratips_this_run = 0
-		var main_node = get_tree().root.get_node_or_null("Main/GameManager")
-		if main_node and main_node.get("p1") and is_instance_valid(main_node.p1):
-			kratips_this_run = main_node.p1.kratips_collected
+		if endless_header_label: endless_header_label.text = "Endless Result"
 			
-		# Update Best Distance
 		if _p1_distance > data.best_distance:
 			data.best_distance = _p1_distance
 			save_data(data)
 			
 		if val_dist_best: val_dist_best.text = "%d m" % data.best_distance
 		if val_run: val_run.text = "%d m" % _p1_distance
-		if val_kratip: val_kratip.text = "%d" % kratips_this_run
+		
+		if winner_name == "Player 1":
+			if endless_winner_tag: endless_winner_tag.texture = tex_p1_tag
+			if endless_avatar: endless_avatar.texture = tex_p1_avatar
+		elif winner_name == "Player 2":
+			if endless_winner_tag: endless_winner_tag.texture = tex_p2_tag
+			if endless_avatar: endless_avatar.texture = tex_p2_avatar
+		else:
+			if endless_winner_tag: endless_winner_tag.texture = null
+			if endless_avatar: endless_avatar.texture = null
+			if endless_winner_container: endless_winner_container.hide()
 		
 	else:
-		if header_label: header_label.text = "Race Result"
-		if winner_container: winner_container.show()
-		if race_score_panel: race_score_panel.show()
-		if endless_vbox: endless_vbox.hide()
+		if endless_ui: endless_ui.hide()
+		if race_ui: race_ui.show()
+		
+		if race_header_label: race_header_label.text = "Race Result"
 		
 		if val_p1_wins: val_p1_wins.text = str(_p1_score)
 		if val_p2_wins: val_p2_wins.text = str(_p2_score)
 		
 		if winner_name == "Player 1":
-			if winner_tag: winner_tag.texture = tex_p1_tag
-			if avatar: avatar.texture = tex_p1_avatar
+			if race_winner_tag: race_winner_tag.texture = tex_p1_tag
+			if race_avatar: race_avatar.texture = tex_p1_avatar
 			data.p1_wins += 1
 		elif winner_name == "Player 2":
-			if winner_tag: winner_tag.texture = tex_p2_tag
-			if avatar: avatar.texture = tex_p2_avatar
+			if race_winner_tag: race_winner_tag.texture = tex_p2_tag
+			if race_avatar: race_avatar.texture = tex_p2_avatar
 			data.p2_wins += 1
 		else:
-			if winner_tag: winner_tag.texture = null
-			if avatar: avatar.texture = null
+			if race_winner_tag: race_winner_tag.texture = null
+			if race_avatar: race_avatar.texture = null
 			
 		save_data(data)
 
-	# Play pop-in animation
 	scale = default_scale * 0.8
 	modulate.a = 0.0
 	show()
